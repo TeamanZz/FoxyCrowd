c9391aba3a2cb661ae00028036fd17c20d9d957f
Alexey <a.yasinko@mail.ru>
2021-09-23 20:50:35 +0300

    Trap animations

--- Assets/Trap.cs
+++ Assets/Trap.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class Trap : MonoBehaviour
 {
     [SerializeField] private GameObject triggerParticles;
+    [SerializeField] private TrapType trapType;
 
     private void OnTriggerEnter(Collider other)
     {
         Fox fox;
         if (other.TryGetComponent<Fox>(out fox))
         {
+            if (trapType == TrapType.Grass)
+                GetComponent<Animator>().Play("Grass Animation");
+            if (trapType == TrapType.Bush)
+                GetComponent<Animator>().Play("Bush Animation");
+
             Instantiate(triggerParticles, gameObject.transform.position, Quaternion.identity);
             Destroy(fox.gameObject);
         }
+    }
+
+    public enum TrapType
+    {
+        Grass,
+        Bush
     }
 }
